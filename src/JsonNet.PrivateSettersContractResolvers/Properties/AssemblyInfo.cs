54aecea35cbc5f61f73a993720a94b05a96bde55
Daniel Wertheim <daniel@wertheim.se>
2018-12-09 21:32:28 +0100

    Converted to .NET Standard 1.3 and 2.0

--- src/JsonNet.PrivateSettersContractResolvers/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("JsonNet.PrivateSettersContractResolvers")]
-[assembly: ComVisible(false)]
-[assembly: Guid("2c473e39-0929-4806-91ea-cf99e369d9dd")]
